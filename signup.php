3536b8bce31df49005839dac40dc542618f65df6
chimpiriudaykiran <udaykiranchimpiri@gmail.com>
2023-11-08 10:44:34 -0600

    Bug fix

--- signup.php
+++ signup.php
@@ -1,16 +1,21 @@
 <?php
 // Handle signup request
-$data = json_decode(file_get_contents("php://input"));
-$conn = new mysqli("localhost", "root", "Nightowl@123", "userdb");
-$username = $data->username;
-$password = password_hash($data->password, PASSWORD_BCRYPT);
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "Nightowl@123");
+define("DB_NAME", "userdb");
 
 // Database connection
-$conn = new mysqli("nightowlspace.com", "root", "Nightowl@123", "userdb");
-var_dump($conn);
-if ($conn->connect_error) {
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($conn->connect_error or $conn == null) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+//Get the input values
+$data = json_decode(file_get_contents("php://input"));
+
+$username = $data->username;
+$password = password_hash($data->password, PASSWORD_BCRYPT);
 
 $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
